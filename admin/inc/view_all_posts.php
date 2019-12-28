76605b20e4c5f77d984beb23367aa589e74e31f8
Khoi Hoang <hello@koicha.dev>
2019-12-28 12:50:22 +0100

    Added a:href on title for view all posts
    - The title should direct you to the correct post website if you click on it

--- admin/inc/view_all_posts.php
+++ admin/inc/view_all_posts.php
@@ -33,8 +33,7 @@
                 echo "<tr>";
                 echo "<td>$post_id</td>";
                 echo "<td>$post_author</td>";
-                echo "<td>$post_title</td>";
-                
+                echo "<td><a href='./../post.php?p_id={$post_id}'>$post_title</a></td>";
                 $query = "SELECT * FROM categories WHERE cat_id = ?";
                 $stmt = mysqli_prepare($connection, $query);
                 mysqli_stmt_bind_param($stmt, "i", $post_category_id);
