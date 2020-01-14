9a79ced86f817b534e218b43192dce51d6578969
Khoi Hoang <hello@koicha.dev>
2020-01-14 17:38:44 +0100

    Koicha Theme: Increase footer Font Awesome icon
    - Increased from default which is fa-xs to fa-lg

--- includes/footer.php
+++ includes/footer.php
@@ -4,13 +4,13 @@
         <div class="col-md-4 col-lg-4 text-center mx-auto my-4">
           <p>🔥 <strong>Designed & Developed by Khoi Hoang</strong> 🔥</p>
           <a type="button" href="https://www.linkedin.com/in/khohoa/" class="btn-floating btn-fb">
-            <i class="fa fa-linkedin-square"></i>
+            <i class="fa fa-linkedin-square fa-lg"></i>
           </a>
           <a type="button" href="https://github.com/KoichaDev" class="btn-floating btn-tw">
-            <i class="fa fa-github" style="width: 1.875rem;"></i> 
+            <i class="fa fa-github fa-lg" style="width: 1.875rem;"></i> 
           </a>
           <a href="mailto: hello@koicha.dev" class="btn-floating btn-mail">
-            <i class="fa fa-envelope"></i>
+            <i class="fa fa-envelope fa-lg"></i>
           </a>
         </div>
       </div>
