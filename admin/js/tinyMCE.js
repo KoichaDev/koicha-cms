f4555973b39c3dd41fdaffa14bafe70e88b278b5
Khoi Hoang <hello@koicha.dev>
2020-01-19 15:36:27 +0100

    Koicha Theme: tinyMCE Added Unordered list

--- admin/js/tinyMCE.js
+++ admin/js/tinyMCE.js
@@ -38,7 +38,7 @@ if (window.location.pathname === '/admin/posts.php') {
         {text: 'PHP', value: 'php'},
     ],
     plugins: 'a11ychecker advcode casechange codesample formatpainter linkchecker image lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-    toolbar: 'a11ycheck addcomment showcomments casechange codesample checklist image code formatpainter insertfile pageembed permanentpen table',
+    toolbar: 'a11ycheck addcomment showcomments numlist bullist casechange codesample checklist image code formatpainter insertfile pageembed permanentpen table',
     content_css: [
       "https://cdnjs.cloudflare.com/ajax/libs/prism/1.6.0/themes/prism-tomorrow.min.css",
       "https://cdnjs.cloudflare.com/ajax/libs/prism/1.6.0/plugins/line-numbers/prism-line-numbers.css",
